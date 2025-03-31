ae8993ce5d6048a48e20af79dc1c7903026d33e4
IronMan-1970 <antihetmandb@gmail.com>
2025-03-31 22:14:28 +0300

    Виправлено: використання змінної PORT

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"go/by/example/restful/api/db"
 	"go/by/example/restful/api/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
 	routes.RegisterRoutes(server)
-	server.Run(":8080") // localhost:8080 (до речі відки беруться порти у сайтах)
+	port := os.Getenv("PORT") // Отримання порту з середовища
+	if port == "" {
+		port = "8080" // Використовується для локального запуску
+	}
+	server.Run(":" + port) // Запускаємо сервер на отриманому порту
 }
