2c9dad848f7c8d1c8d54e2a0d1d311514f082fee
IronMan-1970 <antihetmandb@gmail.com>
2025-04-01 16:09:23 +0300

    Fix: added root route

--- main.go
+++ main.go
@@ -12,13 +12,13 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 	routes.RegisterRoutes(server)
+	server.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "API is running"})
+	})
 	port := os.Getenv("PORT") // Отримання порту з середовища
 	if port == "" {
 		port = "8080" // Використовується для локального запуску
 	}
 	server.Run(":" + port) // Запускаємо сервер на отриманому порту
-  server.GET("/", func(c *gin.Context) {
-    c.JSON(200, gin.H{"message": "API is running"})
-})
 
 }
